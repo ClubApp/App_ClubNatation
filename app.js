98dd0048a3c60591d94f4460b940df0854d23860
ClubApp <clubappnatation@gmail.com>
2017-03-05 20:18:52 +0100

    first user management

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,18 +13,60 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({secret: 'sophielabat',resave: true, saveUninitialized: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// essai management de session
+var sess;
+app.get('/',function(req,res){
+  sess = req.session;
+  if(sess.email) {
+    res.redirect('/logged');
+  }
+ else {
+    res.render('login');
+  }
+});
+
+app.post('/login',function(req,res){
+  sess = req.session;
+//In this we are assigning email to sess.email variable.
+//email comes from HTML page.
+  sess.email=req.body.email;
+  res.end('done');
+});
+
+app.get('/logged',function(req,res){
+  sess = req.session;
+  if(sess.email) {
+   res.write('<h1>Hello '+sess.email+'</h1>');
+   res.end('<a href="/logout">Logout</a>');
+  } else {
+    res.write('  <h1>Please login first.</h1> ');
+    res.end('<a href="/">Login</a>');
+  }
+});
+app.get('/logout',function(req,res){
+req.session.destroy(function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    res.redirect('/');
+  }
+});
+
+});
+
+/*app.use('/', index);
+app.use('/users', users);*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +83,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{ title: 'Error' });
 });
 
 module.exports = app;
